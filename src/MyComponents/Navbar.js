b1dbdc7af7609b4c513472c02aa1deedfe468131
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-04-29 23:43:54 +0530

    Replacing <a> tag with <Link>

--- src/MyComponents/Navbar.js
+++ src/MyComponents/Navbar.js
@@ -7,7 +7,6 @@ export default function Navbar(props) {
 <nav className={`navbar navbar-expand-lg navbar-${props.mode} `} style={{backgroundColor: props.mode==="light" ? "#d4a373" : "#0d1b2a"}}>
   <div className="container-fluid">
     <Link className="navbar-brand" to="#">{props.title}</Link>
-    {/* <a className="navbar-brand" href="/">{props.title}</a> */}
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -15,11 +14,9 @@ export default function Navbar(props) {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/">{props.ghar}</Link>
-          {/* <a className="nav-link active" aria-current="page" href="#!">{props.ghar}</a> */}
         </li>
         <li className="nav-item">
           <Link className="nav-link active" to="/about">About</Link>
-          {/* <a className="nav-link" href="/about">About</a> */}
         </li>
       </ul>
       <div className="form-check form-switch">
